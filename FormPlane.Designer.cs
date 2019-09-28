1f715f5ec71c99c2d284f59703a5a483b874abe8
Алина Викторовна <flamenrage@gmail.com>
2019-09-28 21:15:18 +0400

    github lab1 done

---
+++ FormPlane.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Plane_project
+{
+    partial class FormPlane
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormPlane));
+            this.pictureBoxPlane = new System.Windows.Forms.PictureBox();
+            this.buttonCreate = new System.Windows.Forms.Button();
+            this.buttonUp = new System.Windows.Forms.Button();
+            this.buttonDown = new System.Windows.Forms.Button();
+            this.buttonRight = new System.Windows.Forms.Button();
+            this.buttonLeft = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPlane)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxPlane
+            // 
+            this.pictureBoxPlane.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxPlane.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxPlane.Name = "pictureBoxPlane";
+            this.pictureBoxPlane.Size = new System.Drawing.Size(882, 453);
+            this.pictureBoxPlane.TabIndex = 0;
+            this.pictureBoxPlane.TabStop = false;
+            // 
+            // buttonCreate
+            // 
+            this.buttonCreate.Location = new System.Drawing.Point(15, 15);
+            this.buttonCreate.Name = "buttonCreate";
+            this.buttonCreate.Size = new System.Drawing.Size(86, 31);
+            this.buttonCreate.TabIndex = 1;
+            this.buttonCreate.Text = "Создать";
+            this.buttonCreate.UseVisualStyleBackColor = true;
+            this.buttonCreate.Click += new System.EventHandler(this.ButtonCreate_Click);
+            // 
+            // buttonUp
+            // 
+            this.buttonUp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonUp.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonUp.BackgroundImage")));
+            this.buttonUp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonUp.Location = new System.Drawing.Point(793, 358);
+            this.buttonUp.Name = "buttonUp";
+            this.buttonUp.Size = new System.Drawing.Size(30, 30);
+            this.buttonUp.TabIndex = 2;
+            this.buttonUp.UseVisualStyleBackColor = true;
+            this.buttonUp.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonDown
+            // 
+            this.buttonDown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonDown.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonDown.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonDown.BackgroundImage")));
+            this.buttonDown.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonDown.Location = new System.Drawing.Point(793, 394);
+            this.buttonDown.Name = "buttonDown";
+            this.buttonDown.Size = new System.Drawing.Size(30, 30);
+            this.buttonDown.TabIndex = 3;
+            this.buttonDown.UseVisualStyleBackColor = false;
+            this.buttonDown.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonRight
+            // 
+            this.buttonRight.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonRight.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonRight.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonRight.BackgroundImage")));
+            this.buttonRight.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonRight.Location = new System.Drawing.Point(829, 375);
+            this.buttonRight.Name = "buttonRight";
+            this.buttonRight.Size = new System.Drawing.Size(30, 30);
+            this.buttonRight.TabIndex = 4;
+            this.buttonRight.UseVisualStyleBackColor = false;
+            this.buttonRight.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonLeft
+            // 
+            this.buttonLeft.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonLeft.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonLeft.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonLeft.BackgroundImage")));
+            this.buttonLeft.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonLeft.Location = new System.Drawing.Point(757, 375);
+            this.buttonLeft.Name = "buttonLeft";
+            this.buttonLeft.Size = new System.Drawing.Size(30, 30);
+            this.buttonLeft.TabIndex = 5;
+            this.buttonLeft.UseVisualStyleBackColor = false;
+            this.buttonLeft.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(882, 453);
+            this.Controls.Add(this.buttonLeft);
+            this.Controls.Add(this.buttonRight);
+            this.Controls.Add(this.buttonDown);
+            this.Controls.Add(this.buttonUp);
+            this.Controls.Add(this.buttonCreate);
+            this.Controls.Add(this.pictureBoxPlane);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Бомбардировщик";
+          
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPlane)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxPlane;
+        private System.Windows.Forms.Button buttonCreate;
+        private System.Windows.Forms.Button buttonUp;
+        private System.Windows.Forms.Button buttonDown;
+        private System.Windows.Forms.Button buttonRight;
+        private System.Windows.Forms.Button buttonLeft;
+    }
+}
+
---
+++ FormPlane.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Plane_project
+{
+    public partial class FormPlane : Form
+    {
+        private BomberPlane plane;
+        public FormPlane()
+        {
+            InitializeComponent();
+        }
+        private void Draw()
+        {
+            Bitmap bmp = new Bitmap(pictureBoxPlane.Width, pictureBoxPlane.Height);
+            Graphics gr = Graphics.FromImage(bmp);
+           plane.DrawPlane(gr);
+            pictureBoxPlane.Image = bmp;
+        }
+
+
+        private void ButtonCreate_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            plane = new BomberPlane(rnd.Next(100, 400), rnd.Next(1000, 2100), Color.Gray,
+           Color.Red, true, true);
+            plane.SetPosition(rnd.Next(10, 100), rnd.Next(10, 100), pictureBoxPlane.Width,
+           pictureBoxPlane.Height);
+            Draw();
+        }
+        private void buttonMove_Click(object sender, EventArgs e)
+        {
+            //получаем имя кнопки
+            string name = (sender as Button).Name;
+            switch (name)
+            {
+                case "buttonUp":
+                    plane.MoveTransport(Direction.Up);
+                    break;
+                case "buttonDown":
+                    plane.MoveTransport(Direction.Down);
+                    break;
+                case "buttonLeft":
+                    plane.MoveTransport(Direction.Left);
+                    break;
+                case "buttonRight":
+                    plane.MoveTransport(Direction.Right);
+                    break;
+            }
+            Draw();
+        }
+
+        
+    }
+}
